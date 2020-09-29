9069f7531b298c0ffa7d952e0ae6311905b16388
Duong Dong Quan <dongquan15041@gmail.com>
2020-09-29 23:38:15 +0700

    add java.js

---
+++ java.js
@@ -0,0 +1,78 @@
+a = 3 + 4;
+
+var list1 = [
+  {
+    username: "Duy",
+    password: "12346789",
+  },
+  {
+    username: "Tram",
+    password: "tramnguyen123",
+  },
+  {
+    username: "Ngoc",
+    password: "777",
+  },
+  {
+    username: "Nanh",
+    password: "999",
+  },
+];
+var profile = {
+  name: "Quan",
+  age: 18,
+  hobby: "music,soccer,gaming",
+  isMarried: false,
+  shout: function () {
+    console.log("AHHHH!");
+  },
+};
+var username = prompt("what is your username");
+if (username === "quan") alert("helloo Quân");
+else if (username !== "Quân") {
+  alert("i don't know you");
+}
+var user = null;
+
+function isUserValid(username, password) {
+  for (var i = 0; i < list1.length; i++) {
+    if (username === list1[i].username && password === list1[i].password) {
+      return true;
+    }
+  }
+  return false;
+}
+
+var password = prompt("what is your password");
+
+function signin(user, pass) {
+  if (isUserValid(username, password)) {
+    console.log(profile);
+  } else {
+    alert("sorry, wrong password");
+  }
+}
+
+signin(username, password);
+
+function sing(song) {
+  console.log(song);
+}
+sing("hahahah");
+
+var list = ["djsofji", "leo", "bunny", "buppy", "pig"];
+list.pop();
+list.push("dog");
+var newlist = list.concat(["fog", "chick"]);
+
+list.sort();
+
+for (var i = 0; i < newlist.length; i += 2) {
+  console.log(newlist[i]);
+}
+
+var button = document.getElementsByTagName("button")[0];
+
+button.addEventListener("click", function(){
+	console.log("click!");
+})
