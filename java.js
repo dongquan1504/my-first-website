b80c3fd5cee225d5b33500f0b2e517adfefea1be
Duong Dong Quan <dongquan15041@gmail.com>
2020-09-30 17:25:34 +0700

    Update files
    Author:     Duong Dong Quan <dongquan15041@gmail.com>

--- java.js
+++ java.js
@@ -71,8 +71,34 @@ for (var i = 0; i < newlist.length; i += 2) {
   console.log(newlist[i]);
 }
 
-var button = document.getElementsByTagName("button")[0];
+//var button = document.getElementsByTagName("button")[0];
+//button.addEventListener("click", function(){
+//	console.log("click!");
+//})
 
-button.addEventListener("click", function(){
-	console.log("click!");
-})
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+
+function inputLength() {
+  return input.value.length;
+}
+function createListElement() {
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
+}
+function addListAfterClick() {
+  if (inputLength() > 0) {
+    createListElement();
+  }
+}
+function addListAfterKeypress(event) {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
+  }
+}
+button.addEventListener("click", addListAfterClick);
+
+input.addEventListener("keypress", addListAfterKeypress);
